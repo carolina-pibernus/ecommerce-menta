a37e71a88db36be92d62837898bc089b42be3d65
carolina-pibernus <carolinapibernus@gmail.com>
2021-03-23 11:49:02 -0300

    Primer commit del proyecto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MENTA</h1>
+        <h2>BAZAR ONLINE</h2>
+        <p className="parrafo">
+          Conseguí de todo para poner linda tu cocina
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
