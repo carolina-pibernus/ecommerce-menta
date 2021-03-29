0f12c520e6d007c74a4f7f88d2bd08a95d6b3a20
carolina-pibernus <carolinapibernus@gmail.com>
2021-03-29 15:48:07 -0300

    Incorporacion de barra de navegación

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>MENTA</h1>
-        <h2>BAZAR ONLINE</h2>
         <p className="parrafo">
           Conseguí de todo para poner linda tu cocina
         </p>
-        
+        <nav> 
+          <ul>
+            <li><a href="https://www.google.com/">HOME</a></li>
+            <li><a href="https://www.google.com/">VAJILLA</a></li>
+            <li><a href="https://www.google.com/">UTENSILIOS</a></li>
+            <li><a href="https://www.google.com/">DECO</a></li>
+            <li><a href="https://www.google.com/">ACCESORIOS</a></li>
+            <li><a href="https://www.google.com/">CONTACTO</a></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
